68cf197cd7dd778faa5fc1059e11bd87d62a1734
Uvacoder <uva22940coder@gmail.com>
2022-09-24 13:28:09 -0400

    Update footer.blade.php

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -7,8 +7,8 @@
             </div>
             <!-- Required Unicons (if you want) -->
             <div class="flex items-center space-x-3 text-sm">
-                <div class="hidden md:block">Follow us: </div>
-                <a href="https://twitter.com/AlecCam43544378" class="flex items-center mx-1 space-x-2">
+                <div class="hidden md:block">Follow me: </div>
+                <a href="https://twitter.com/VaLandscapes" class="flex items-center mx-1 space-x-2">
                     <img src="/assets/images/twitter.svg" alt="twitter">
                     <div>AlecCam43544378</div>
                 </a>
@@ -16,14 +16,7 @@
                     <img src="/assets/images/github.svg" alt="github">
                     <div>uvacoder</div>
                 </a>
-                <a href="https://www.youtube.com/user/TechGuyWeb" class="flex items-center mx-1 space-x-2 text-sm">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <rect x="3" y="5" width="18" height="14" rx="4" />
-                        <path d="M10 9l5 3l-5 3z" />
-                    </svg>
-                    <span>uvacoder</span>
-                </a>                
+                                
             </div>
         </div>    
 </footer>
