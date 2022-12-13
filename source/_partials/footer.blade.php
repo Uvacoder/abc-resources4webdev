15876ed451b9821d841c254429b834af2b40eace
uvacoder <uva22940coder@gmail.com>
2022-12-12 20:54:40 -0500

    Update footer.blade.php

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- component -->
-<footer class="bg-blue-400">    
+<footer class="bg-blue-100">    
     <!-- Copyright Bar -->    
-        <div class="flex flex-col items-center justify-between max-w-6xl px-3 py-4 m-auto text-xl text-gray-400 md:flex-row">
+        <div class="flex flex-col items-center justify-between max-w-6xl px-3 py-4 m-auto text-xl text-white md:flex-row">
             <div class="text-sm">
                 © Copyright {{ date('Y') }}. All Rights Reserved.
             </div>
